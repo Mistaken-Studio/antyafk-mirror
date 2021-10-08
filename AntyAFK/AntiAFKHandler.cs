5c80f9111bb856cdc62417d1602578f9e0a5f5d3
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-10-08 08:01:57 +0200

    Dodaj pliki projektów.

---
+++ AntyAFK/AntiAFKHandler.cs
@@ -0,0 +1,152 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AntiAFKHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
+using MEC;
+using Mistaken.API;
+using Mistaken.API.Commands;
+using Mistaken.API.Diagnostics;
+using Mistaken.API.Extensions;
+using Mistaken.API.GUI;
+using Mistaken.RoundLogger;
+using UnityEngine;
+
+namespace Mistaken.AntyAFK
+{
+    /// <inheritdoc/>
+    public class AntiAFKHandler : Module
+    {
+        /// <inheritdoc cref="Module.Module(Exiled.API.Interfaces.IPlugin{Exiled.API.Interfaces.IConfig})"/>
+        public AntiAFKHandler(PluginHandler p)
+            : base(p)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override bool IsBasic => true;
+
+        /// <inheritdoc/>
+        public override string Name => "AntiAFK";
+
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += this.Handle(() => this.Server_RoundStarted(), "RoundStart");
+            Exiled.Events.Handlers.Server.RestartingRound += this.Handle(() => this.Server_RestartingRound(), "RoundRestart");
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= this.Handle(() => this.Server_RoundStarted(), "RoundStart");
+            Exiled.Events.Handlers.Server.RestartingRound -= this.Handle(() => this.Server_RestartingRound(), "RoundRestart");
+        }
+
+        internal static readonly Dictionary<int, (int value, Vector3 lastPosition)> AfkPosition = new Dictionary<int, (int value, Vector3 lastPosition)>();
+
+        private const string AfkMessage =
+            @"<size=40><voffset=20em>
+            <color=red><b><size=200>WARNING</size></b></color>
+            <br><br><br><br><br>
+            You have <color=yellow>{sLeft} seconds</color> to move or type '<color=yellow>.notafk</color>' in console('<color=yellow>~</color>')<br>
+            or you will be kicked by AntyAFK system.
+            </voffset></size>";
+
+        private void Server_RestartingRound()
+        {
+            AfkPosition.Clear();
+        }
+
+        private void Server_RoundStarted()
+        {
+            this.RunCoroutine(this.AfkDetector(), "AfkDetector");
+        }
+
+        private IEnumerator<float> AfkDetector()
+        {
+            yield return Timing.WaitForSeconds(1);
+            int rid = RoundPlus.RoundId;
+            while (Round.IsStarted && rid == RoundPlus.RoundId)
+            {
+                this.CheckForAfk(1);
+                yield return Timing.WaitForSeconds(1);
+            }
+        }
+
+        private void CheckForAfk(int seconds)
+        {
+            foreach (var player in RealPlayers.List.Where(p => p.IsAlive && p.Role != RoleType.Scp079 && !p.GetEffectActive<CustomPlayerEffects.Ensnared>()).ToArray())
+            {
+                if (player.CheckPermission(PluginHandler.Instance.Name + ".anti_afk_kick_proof") && false)
+                    continue;
+                var ppos = player.Position;
+                if (AfkPosition.TryGetValue(player.Id, out var value))
+                {
+                    int level = value.value;
+                    Vector3 pos = value.lastPosition;
+                    if (pos.x == ppos.x && pos.y == ppos.y && pos.z == ppos.z)
+                    {
+                        if (player.GetSessionVar<bool>(SessionVarType.TALK))
+                            continue;
+
+                        AfkPosition[player.Id] = (level + seconds, ppos);
+
+                        switch (level + 1)
+                        {
+                            case 120:
+                                {
+                                    RLogger.Log("ANTY AFK", "WARN", $"{player.PlayerToString()} was warned for being afk");
+                                    this.RunCoroutine(this.InformAFK(player), "InformAFK");
+
+                                    break;
+                                }
+
+                            case 180:
+                                {
+                                    player.Disconnect("Anti AFK: You were AFK");
+                                    RLogger.Log("ANTY AFK", "DISCONNECT", $"{player.PlayerToString()} was disconnected for being afk");
+                                    MapPlus.Broadcast("Anti AFK", 10, $"{player.Nickname} was disconnected for being AFK", Broadcast.BroadcastFlags.AdminChat);
+
+                                    break;
+                                }
+                        }
+                    }
+                    else
+                        AfkPosition[player.Id] = (0, ppos);
+                }
+                else
+                    AfkPosition.Add(player.Id, (0, ppos));
+            }
+        }
+
+        private IEnumerator<float> InformAFK(Player p)
+        {
+            PseudoGUIHandler.Ignore(p);
+            for (int i = 60; i > -1; i--)
+            {
+                if (AfkPosition.TryGetValue(p.Id, out var value))
+                {
+                    if (value.value >= 120)
+                        p.ShowHint(AfkMessage.Replace("{sLeft}", i.ToString("00")), 2);
+                    else
+                    {
+                        PseudoGUIHandler.StopIgnore(p);
+                        yield break;
+                    }
+                }
+                else
+                {
+                    PseudoGUIHandler.StopIgnore(p);
+                    yield break;
+                }
+
+                yield return Timing.WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ AntyAFK/CommandHandler.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AntiAFKHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
+using MEC;
+using Mistaken.API;
+using Mistaken.API.Commands;
+using Mistaken.API.Diagnostics;
+using Mistaken.API.Extensions;
+using Mistaken.API.GUI;
+using Mistaken.RoundLogger;
+using UnityEngine;
+
+namespace Mistaken.AntyAFK
+{
+    /// <inheritdoc/>
+    [CommandSystem.CommandHandler(typeof(CommandSystem.ClientCommandHandler))]
+    public class CommandHandler : IBetterCommand
+    {
+        /// <inheritdoc/>
+        public override string Command => "notafk";
+
+        /// <inheritdoc/>
+        public override string[] Aliases => new string[] { };
+
+        /// <inheritdoc/>
+        public override string Description => "I'm not afk";
+
+        /// <inheritdoc/>
+        public override string[] Execute(global::CommandSystem.ICommandSender sender, string[] args, out bool success)
+        {
+            var player = sender.GetPlayer();
+            if (AntiAFKHandler.AfkPosition.ContainsKey(player.Id))
+                AntiAFKHandler.AfkPosition.Remove(player.Id);
+            success = true;
+            return new string[] { "Done" };
+        }
+    }
+}
